15f267dd126baa8b7818fdc83675f9ed306dc533
Riday1 <rrriday100@gmail.com>
2022-10-06 12:07:59 +0600

    color attribute is added

--- src/App.js
+++ src/App.js
@@ -11,14 +11,14 @@ const productstyle = {
 }
 function App() {
   const products = [
-    { name: 'laptop', price: 70000 },
-    { name: 'iphone 11', price: 34000 },
-    { name: 'Computer', price: 29999 },
-    { name: 'Su-35', price: 10000 },
-    { name: 'apache helicoper', price: 20000 },
-    { name: 'nuclear Boomb', price: 30000 },
-    { name: 'j-20', price: 5000 },
-    { name: "vt-5 light tank", price: 20000 }
+    { name: 'laptop', price: 70000, color: 'black' },
+    { name: 'iphone 11', price: 34000, color: 'black' },
+    { name: 'Computer', price: 29999, color: 'black' },
+    { name: 'Su-35', price: 10000, color: 'black' },
+    { name: 'apache helicoper', price: 20000, color: 'black' },
+    { name: 'nuclear Boomb', price: 30000, color: 'black' },
+    { name: 'j-20', price: 5000, color: 'black' },
+    { name: "vt-5 light tank", price: 20000, color: 'black' }
   ]
   return (
     <div className="App">
@@ -29,7 +29,7 @@ function App() {
       <Product name="Su-35" price="10000"></Product> */}
 
       {
-        products.map(product => <Product name={product.name} price={product.price}></Product>)
+        products.map(product => <Product name={product.name} price={product.price} color={product.color}></Product>)
       }
 
     </div>
@@ -44,6 +44,7 @@ function Product(props) {
     <div style={productstyle}>
       <h3>Product Name : {props.name} </h3>
       <p>Product Price : {props.price}</p>
+      <p>Product Color : {props.color}</p>
     </div>
   )
 }
