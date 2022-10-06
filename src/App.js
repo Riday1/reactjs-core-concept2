8252ec35655904122e4b8c0627028517764e5ca0
Riday1 <rrriday100@gmail.com>
2022-10-06 12:27:14 +0600

    Aircrafts array is added

--- src/App.js
+++ src/App.js
@@ -10,11 +10,20 @@ const productstyle = {
 
 }
 function App() {
+  const Aircrafts = [
+    { name: "Su-35", price: "1000", color: "cemoFlag", origin: "Russia" },
+    { name: "Rafale", price: "2000", color: "gray", origin: "Franch" },
+    { name: "Tyfun", price: "1500", color: "light gray", origin: "Eupore" },
+    { name: "Birakter Tb-2", price: "10", color: "skey blue", origin: "Tukiye" },
+    { name: "Mig-29", price: "1800", color: "comb. of blue and Dark blue", origin: "Russian" }
+  ]
 
   return (
     <div className="App">
 
-      <Product></Product>
+      {
+        Aircrafts.map(aricraft => <Product name={Aircrafts.name} price={Aircrafts.price} color={Aircrafts.color}></Product>)
+      }
 
     </div>
   );
@@ -22,13 +31,17 @@ function App() {
 
 
 
-function Product() {
+function Product(props) {
+  // console.log(props)
   return (
 
-    <div>
-      <h3>Product Name : Su - 35</h3>
-      <p>Product Price : 20000</p>
-    </div>
+    <div className='product'>
+      <h1>Product Name : {props.name} </h1>
+      <h3>Product Price : {props.price}</h3>
+      <h3>Product color : {props.color}</h3>
+      <p>Origin : { }</p>
+
+    </div >
   )
 }
 
